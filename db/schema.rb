ae40122caa85f40d96674fe2074505b907153501
grassme <lanh.ptit.tn@gmail.com>
2018-02-22 15:39:36 +0700

    chap9: advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180111065433) do
+ActiveRecord::Schema.define(version: 20180222085755) do
 
   create_table "schools", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180111065433) do
     t.string "password_digest"
     t.integer "school_id"
     t.date "dob"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["school_id"], name: "index_users_on_school_id"
   end
